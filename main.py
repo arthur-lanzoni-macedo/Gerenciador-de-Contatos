4e3b82e12b2b7a1bb8353cc3f705a8dc9332bcfc
arthur-lanzoni-macedo <Arthur.lanzoni@gmail.com>
2026-01-27 22:50:13 -0300

    Adicionado opção de remover e buscar contatos

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ lista_de_contatos = []
 # TITULO
 def titulo_projeto():
     os.system("cls")
-    print(("𝒢ℰℛℰ𝒩𝒞ℐ𝒜𝒟𝒪ℛ 𝒟ℰ 𝒞𝒪𝒩𝒯𝒜𝒯𝒪𝒮\n"))    
+    print(("𝑮𝑬𝑹𝑬𝑵𝑪𝑰𝑨𝑫𝑶 𝑫𝑬 𝑪𝑶𝑵𝑻𝑨𝑻𝑶𝑺\n"))    
 
 # MENU DE OPÇÕES
 def opcao_menu():
@@ -28,9 +28,9 @@ def escolha_das_opcoes():
             elif escolha == 2:
                 listar_contato()
             elif escolha == 3:
-                print("teste 3")
+                buscar_contato()
             elif escolha == 4:
-                print("teste 4")
+                remover_contato()
             elif escolha == 5:
                 print("teste 5")
             elif escolha == 6:
@@ -98,7 +98,6 @@ def adicionar_contato():
 def listar_contato():
     titulos("𝑳𝒊𝒔𝒕𝒂 𝒅𝒆 𝑪𝒐𝒏𝒕𝒂𝒕𝒐𝒔")
     
-    
     if not lista_de_contatos:
         print("Sua agenda parece um pouco solitária... 👤✨")
         print("Que tal adicionar o primeiro contato para começar a sua rede?")    
@@ -106,9 +105,71 @@ def listar_contato():
         print(f"Total de conexões salvas: {len(lista_de_contatos)} 📱\n")
         
         for numero, tarefa in enumerate(lista_de_contatos, start=1): 
-            print(f"{numero:02d} → {tarefa}") 
-            print("--------------------")
-    
+            print(f"{numero:02d} → \n👤 Nome: {tarefa['nome:']}"
+                    f"\n📞 Telefone: {tarefa['telefone:']}"
+                    f"\n📧 Email: {tarefa['email:']}"
+                    f"\n{'-'*30}")    
     voltar()
-   
+
+# BUSCAR CONTATOS
+def buscar_contato():
+    titulos("𝑩𝒖𝒔𝒄𝒂𝒓 𝑪𝒐𝒏𝒕𝒂𝒕𝒐")
+    
+    if not lista_de_contatos:
+        print("\n🌟 Ops! Parece que sua lista de contatos ainda está em branco. 🏜️")
+        print("Que tal adicionar alguém para dar vida a ela? ✨")
+        print("-" * 30)
+    else:
+        
+        buscar = input("Digite o nome que deseja buscar: ").lower()
+        encontrado = False
+
+        for contato in lista_de_contatos:
+            if buscar in contato["nome:"].lower():
+                print(
+                    f"\n👤 Nome: {contato['nome:']}"
+                    f"\n📞 Telefone: {contato['telefone:']}"
+                    f"\n📧 Email: {contato['email:']}"
+                    f"\n{'-'*30}"
+                )
+                encontrado = True
+
+        if not encontrado:
+            print(f"\n🔍 Ih! Procurei por '{buscar}', mas não encontrei ninguém com esse nome. 🕵️‍♂️")
+            print("Verifique se digitou o nome certinho ou tente buscar apenas uma parte dele! ✨")
+            print("-" * 35)
+                
+    voltar()
+
+# REMOVER CONTATO
+def remover_contato():
+    titulos("𝑹𝒆𝒎𝒐𝒗𝒆𝒓 𝑪𝒐𝒏𝒕𝒂𝒕𝒐")
+
+    if not lista_de_contatos:
+        print("\n📭 Nenhum contato para remover.")
+        voltar()
+        return
+    else:
+        
+        for i, contato in enumerate(lista_de_contatos, start=1):
+            print(f"{i}: Nome: {contato['nome:']}")
+         
+    escolher_removido = input("\nDigite o número do contato que deseja remover: ")
+         
+    if not escolher_removido:
+        print("\n🚫 Digite um número válido!")
+        voltar()
+        return   
+    
+    escolher_removido = int(escolher_removido)
+    
+    if escolher_removido >= 1 and escolher_removido <= len(lista_de_contatos):
+        removido = lista_de_contatos.pop(escolher_removido - 1)
+        print(f"\n🗑️ Contato '{removido['nome:']}' removido com sucesso!")
+    else:
+        print("\n🚫 Esse número não existe na lista.")
+
+    voltar()      
+
+
 visualizar_projeto()
